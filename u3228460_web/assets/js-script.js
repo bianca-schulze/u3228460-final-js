72eff79b0d7c7f5252290d3a149882c1f5f5e49c
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2022-10-23 21:59:37 +1100

    Added files

---
+++ u3228460_web/assets/js-script.js
@@ -0,0 +1,172 @@
+
+
+
+// slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+setInterval(showSlides, 3000)
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  if (slideIndex < slides.length) {slideIndex++}
+  else {slideIndex = 1}
+}
+
+// fire 
+
+function attachFlame() {
+  var flame = document.querySelector(':root');
+  flame.style.setProperty('--position', 'absolute');
+  flame.style.setProperty('--bottom', '-118.5em');
+}
+
+function dettachFlame() {
+  var flame = document.querySelector(':root');
+  flame.style.setProperty('--position', 'fixed');
+  flame.style.setProperty('--bottom', '0%');
+}
+
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+      var scrollpos = $(this).scrollTop();
+      console.log($(this).scrollTop())
+      if(scrollpos < 19023){
+        dettachFlame()
+      }
+      else {
+        attachFlame()
+      }
+      if(scrollpos >= 100 && scrollpos < 200){
+        // set to new image
+        $(".flame-change img").attr("src", "assets/images/flame-2");
+      }
+      else if(scrollpos >= 200 && scrollpos < 300){
+        $(".flame-change img").attr("src", "assets/images/flame-3");
+      }  
+      else if(scrollpos >= 300 && scrollpos < 400){
+        $(".flame-change img").attr("src", "assets/images/flame-4");
+      }  
+      else if(scrollpos >= 400 && scrollpos < 500){
+        $(".flame-change img").attr("src", "assets/images/flame-5");
+      }  
+      else if(scrollpos >= 500 && scrollpos < 600){
+        $(".flame-change img").attr("src", "assets/images/flame-6");
+      }  
+      else if(scrollpos >= 600 && scrollpos < 700){
+        $(".flame-change img").attr("src", "assets/images/flame-7");
+      }  
+      else if(scrollpos >= 700 && scrollpos < 800){
+        $(".flame-change img").attr("src", "assets/images/flame-8");
+      }  
+      else if(scrollpos >= 800 && scrollpos < 900){
+        $(".flame-change img").attr("src", "assets/images/flame-9");
+      }  
+      else if(scrollpos >= 900 && scrollpos < 1000){
+        $(".flame-change img").attr("src", "assets/images/flame-10");
+      }  
+      else if(scrollpos >= 1000 && scrollpos < 1100){
+        $(".flame-change img").attr("src", "assets/images/flame-11");
+      }  
+      else if(scrollpos >= 1100 && scrollpos < 1200){
+        $(".flame-change img").attr("src", "assets/images/flame-12");
+      }  
+      else if(scrollpos >= 1200 && scrollpos < 1300){
+        $(".flame-change img").attr("src", "assets/images/flame-13");
+      } 
+      else if(scrollpos >= 1300 && scrollpos < 1400){
+        $(".flame-change img").attr("src", "assets/images/flame-14");
+      }  
+      else if(scrollpos >= 1400 && scrollpos < 1500){
+        $(".flame-change img").attr("src", "assets/images/flame-15");
+      }  
+      else if(scrollpos >= 1500 && scrollpos < 1600){
+        $(".flame-change img").attr("src", "assets/images/flame-16");
+      } 
+      else if(scrollpos >= 1600 && scrollpos < 1700){
+        $(".flame-change img").attr("src", "assets/images/flame-17");
+      }  
+      else if(scrollpos >= 1700 && scrollpos < 1800){
+        $(".flame-change img").attr("src", "assets/images/flame-18");
+      }  
+      else if(scrollpos >= 1800 && scrollpos < 1900){
+        $(".flame-change img").attr("src", "assets/images/flame-19");
+      }  
+      else if(scrollpos >= 1900 && scrollpos < 2000){
+        $(".flame-change img").attr("src", "assets/images/flame-20");
+      }  
+      else if(scrollpos >= 2000 && scrollpos < 2100){
+        $(".flame-change img").attr("src", "assets/images/flame-21");
+      }  
+      else if(scrollpos >= 2100 && scrollpos < 2150){
+        $(".flame-change img").attr("src", "assets/images/flame-22");
+      }  
+      else if(scrollpos >= 2150 && scrollpos < 2200){
+        $(".flame-change img").attr("src", "assets/images/flame-23");
+      }  
+      else if(scrollpos >= 2200 && scrollpos < 2250){
+        $(".flame-change img").attr("src", "assets/images/flame-24");
+      }  
+      else if(scrollpos >= 2250){
+        $(".flame-change img").attr("src", "assets/images/flame-25");
+      }   
+      else {
+        //back to default
+        $(".flame-change img").attr("src", "assets/images/flame-1");
+      }
+    })
+  })
+
+  // modal 
+
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+ 
---
+++ u3228460_web/readme.md
@@ -0,0 +1,46 @@
+# Welcome to my site 
+
+The intention of my site is to raise awareness around bushfires in Australia. 
+
+As there was no set sizing for this assignment so I built based on the dimensions: **1280px × 720px** 
+
+## Preview: 
+
+[![IMAGE_ALT](https://img.youtube.com/vi/UTh2xntGs7k/0.jpg)](https://www.youtube.com/watch?v=UTh2xntGs7k)
+
+## Challenges 
+### JavaScript 
+JavaScript is very new to me, so to achieve what I was able to do took a time, dedication and effort. It is like trying to learn another language, if you don't fully understand it, it feels impossible to write effectively using the language. As a result, I have managed to create some effects with JavaScript, however the code is not as efficient as it could be. 
+
+### Responsiveness 
+I understand the importance of creating responsive websites. Unfortunately for this assignment, I needed to spent  majority of my time trying to get the code to work which left me no time to improve the responsivness and accessibility of the site. 
+
+### Technical limitations 
+There are a number of elements that I would have loved to animate, however given my limited understading and short amount of time, I was unable to achieve some of the things I would have liked. 
+
+I would have liked to include:
+- **Scroll and slide effects**, where as you scroll thorugh the fire safety instructions, the icons slide in from the right and the paragraphs fade in on scroll. 
+- **Pop-up elements**, where you can hover over the statistics to show charts or graphs making this section more engaging. 
+- **Different layouts**, instead of a standard page scroll I would have liked to code a site that takes the user on a bit of a "journey", rather than making them scroll tirelessly to the bottom.  
+
+## Strengths 
+### HTML and CSS 
+Having prior understanding of HTML and CSS and a realitve level of comfort with their syntax, allowed me to straight away dive in and create the basis of the site. It also allow me to create some animation using CSS. 
+
+### Troubleshooting 
+I am quite comfortable using insepct mode within the browser which made it easy for me to diagnose and fix small issues within the site. 
+
+### Design 
+Being a design students makes it quite easy for me to ideate and plan what my site will look like. I did not have any issues when designing elements, colours and type for the site. 
+
+## References 
+### Inspirations
+- **Slideshow** (lines 43-76 in index.html, 162-227 in style.css and 4-33 in js-script.js) inspired by the code found at [W3 Schools](https://www.w3schools.com/howto/howto_js_slideshow.asp).
+- **Flame change on scroll** (lines 180-182 in index.html, 127-131 in style.ccs and 35-136 in js-script.js) inspired by the code found at [CodePen](https://codepen.io/Spoochy/pen/LYYdGgJ).
+- **Modal** (lines 27-39 in index.html, 243-272 in style.css and 138-168 in js-script.js) inspired by the code found at [W3 Schools](https://www.w3schools.com/howto/howto_css_modals.asp).
+
+### Content
+- **O’Donnell, J.** (2021) *Major bushfires in Australian history - the 1974 and 1975 Australian bushfires I Australian Rural &amp; Regional News, Australian Rural &amp; Regional News. Available at: https://arr.news/2021/10/28/major-bushfires-in-australian-history-the-1974-and-1975-australian-bushfires/* (Accessed: October 23, 2022). 
+
+- **Rotton, T.** (2021) *2019-2020 Australian bushfires, Center for Disaster Philanthropy. Available at: https://disasterphilanthropy.org/disasters/2019-australian-wildfires/* (Accessed: October 23, 2022). 
+
